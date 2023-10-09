a1eb07cfab970613f6cc1091d6059427cd8afee5
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-09 08:57:55 +0600

    implemented user login

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller()
 export class UsersController {
@@ -19,6 +20,15 @@ export class UsersController {
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.usersService.createUser(createUserDto);
+    return this.usersService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  public async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.usersService.signin(loginUserDto);
     return this.usersService.buildUserResponse(user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,7 +6,12 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/jwt.config';
 import { UserResponseInterface } from './types/userResponse.interface';
-import { EMAIL_OR_USERNAME_TAKEN_ERROR } from './users.constants';
+import {
+  EMAIL_OR_USERNAME_TAKEN_ERROR,
+  USER_CREDENTIALS_ERROR,
+} from './users.constants';
+import { LoginUserDto } from './dto/login-user.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -34,6 +39,36 @@ export class UsersService {
     Object.assign(newUser, createUserDto);
 
     return await this.userRepository.save(newUser);
+  }
+
+  public async signin(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: { email: loginUserDto.email },
+      select: ['id', 'username', 'email', 'bio', 'image', 'password'],
+    });
+
+    if (!user) {
+      throw new HttpException(
+        USER_CREDENTIALS_ERROR,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const isPasswordCorrect = await compare(
+      loginUserDto.password,
+      user.password,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        USER_CREDENTIALS_ERROR,
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete user.password;
+
+    return user;
   }
 
   public generateJwt(user: UserEntity): string {
