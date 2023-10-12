7080d9c6c0b2df88230a10b4bdfb4614cbfc3761
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-12 12:27:31 +0600

    implemented the follow user feature

--- src/profiles/profiles.constants.ts
+++ src/profiles/profiles.constants.ts
@@ -1,1 +1,3 @@
 export const PROFILE_DOES_NOT_EXIST_ERROR = 'Profile does not exist';
+export const PROFILE_BAD_REQUEST_ERROR =
+  "Follower and following can't be equal";
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -4,13 +4,19 @@ import { UserEntity } from '../users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { ProfileType } from './types/profile.type';
 import { ProfileResponseInterface } from './types/profile-response.interface';
-import { PROFILE_DOES_NOT_EXIST_ERROR } from './profiles.constants';
+import {
+  PROFILE_BAD_REQUEST_ERROR,
+  PROFILE_DOES_NOT_EXIST_ERROR,
+} from './profiles.constants';
+import { FollowEntity } from './entities/follow.entity';
 
 @Injectable()
 export class ProfilesService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   public async findProfileByUsername(
@@ -29,6 +35,46 @@ export class ProfilesService {
     }
 
     return { ...user, following: false };
+  }
+
+  public async followProfileByUsername(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException(
+        PROFILE_DOES_NOT_EXIST_ERROR,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        PROFILE_BAD_REQUEST_ERROR,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: {
+        followerId: currentUserId,
+        followingId: user.id,
+      },
+    });
+
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
   }
 
   public buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
