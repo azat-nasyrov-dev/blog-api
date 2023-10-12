45d8d5f4160905b3d309b31158202ee1cdccfa3b
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-12 11:44:22 +0600

    added migration

---
+++ src/migrations/1697089349610-CreateFollows.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateFollows1697089349610 implements MigrationInterface {
+  name = 'CreateFollows1697089349610';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "follows" ("id" SERIAL NOT NULL, "followerId" integer NOT NULL, "followingId" integer NOT NULL, CONSTRAINT "PK_8988f607744e16ff79da3b8a627" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "follows"`);
+  }
+}
