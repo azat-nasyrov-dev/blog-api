1beb238ebd8f576857f4a943de961dea3fe923c1
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-12 16:33:37 +0600

    solved the problem with cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
 
   await app.listen(port, () => console.log(`Server started on port: ${port}`));
