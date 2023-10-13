43e9dfa1f382a4a385bfbb7994d1e3faf3ac8919
Azat Nasyrov <azat.nasyrov31@gmail.com>
2023-10-13 18:03:09 +0600

    added documentation to the tags module via swagger

--- src/tags/entities/tag.entity.ts
+++ src/tags/entities/tag.entity.ts
@@ -1,10 +1,13 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tags' })
 export class TagEntity {
+  @ApiProperty({ example: 1, description: 'Unique identifier' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 'name', description: 'Tag name' })
   @Column()
   name: string;
 }
