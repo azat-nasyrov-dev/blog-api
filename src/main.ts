c4eaff540a2436d3e8f61c3b728d07eb8c4c8465
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-27 22:24:10 +0600

    modified main file

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+
+  await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
 
 bootstrap();
