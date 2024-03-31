2cdbb7ee6c46097803c2250d4e9cc697204bf214
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-03-31 20:24:03 +0600

    implemented another feature favorited by user

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -63,6 +63,21 @@ export class ArticlesService {
       queryBuilder.andWhere('articles.authorId = :id', { id: author.id });
     }
 
+    if (query.favorited) {
+      const author = await this.userRepository.findOne({
+        where: { username: query.favorited },
+        relations: ['favorites'],
+      });
+
+      const ids = author.favorites.map((element) => element.id);
+
+      if (ids.length > 0) {
+        queryBuilder.andWhere('articles.id IN (:...ids)', { ids });
+      } else {
+        queryBuilder.andWhere('1=0');
+      }
+    }
+
     if (query.limit) {
       queryBuilder.limit(query.limit);
     }
@@ -71,9 +86,25 @@ export class ArticlesService {
       queryBuilder.offset(query.offset);
     }
 
-    const articles = await queryBuilder.getMany();
+    let favoriteIds: number[] = [];
 
-    return { articles, articlesCount };
+    if (currentUserId) {
+      const currentUser = await this.userRepository.findOne({
+        where: { id: currentUserId },
+        relations: ['favorites'],
+      });
+
+      favoriteIds = currentUser.favorites.map((favorite) => favorite.id);
+    }
+
+    const articles = await queryBuilder.getMany();
+    const articlesWithFavorites = articles.map((article) => {
+      const favorited = favoriteIds.includes(article.id);
+
+      return { ...article, favorited };
+    });
+
+    return { articles: articlesWithFavorites, articlesCount };
   }
 
   public async findBySlug(slug: string): Promise<ArticleEntity> {
