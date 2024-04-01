f424b65745e9f5dc632ab528d6401bbd5e8a577e
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-04-02 01:46:25 +0600

    added and configured swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Blog-API')
+    .setDescription('Blog-API Documentation')
+    .setVersion('1.0')
+    .setBasePath('api')
+    .addBearerAuth()
+    .addTag('Developed by Azat Nasyrov')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 }
