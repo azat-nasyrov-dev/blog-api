e972df1fefc6ff32fa18b464fd80d83440696f02
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-04-02 02:16:38 +0600

    added documentation to the tags via swagger

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { TagsService } from './tags.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Tags')
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
+  @ApiOperation({ summary: 'Get all tags' })
+  @ApiResponse({ status: 200, description: 'Return all tags' })
   @Get()
   public async findAll(): Promise<{ tags: string[] }> {
     const tags = await this.tagsService.findListOfTags();
