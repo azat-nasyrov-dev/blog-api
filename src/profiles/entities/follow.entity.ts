11439bd7eea939b4dd43d2dacdb6d8732ad97ea4
Azat Nasyrov <azat.nasyrov31@gmail.com>
2024-04-02 02:46:49 +0600

    added documentation to the profiles via swagger

--- src/profiles/entities/follow.entity.ts
+++ src/profiles/entities/follow.entity.ts
@@ -1,13 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'follows' })
 export class FollowEntity {
+  @ApiProperty({ example: 1, description: 'Unique identifier' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ example: 3, description: 'Follower ID' })
   @Column({ name: 'follower_id' })
   followerId: number;
 
+  @ApiProperty({ example: 4, description: 'Following ID' })
   @Column({ name: 'following_id' })
   followingId: number;
 }
--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -3,11 +3,16 @@ import { User } from '../users/decorators/user.decorator';
 import { ProfileResponseInterface } from './types/profile-response.interface';
 import { ProfilesService } from './profiles.service';
 import { AuthGuard } from '../users/guards/auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Profiles')
 @Controller('profiles')
 export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
+  @ApiOperation({ summary: 'Get profile' })
+  @ApiResponse({ status: 200, description: 'Return profile' })
   @Get(':username')
   public async getProfile(
     @User('id') currentUserId: number,
@@ -20,6 +25,11 @@ export class ProfilesController {
     return this.profilesService.buildProfileResponse(profile);
   }
 
+  @ApiOperation({ summary: 'Follow profile' })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully followed profile',
+  })
   @Post(':username/follow')
   @UseGuards(AuthGuard)
   public async followProfile(
@@ -33,6 +43,11 @@ export class ProfilesController {
     return this.profilesService.buildProfileResponse(profile);
   }
 
+  @ApiOperation({ summary: 'Unfollow profile' })
+  @ApiResponse({
+    status: 201,
+    description: 'The user has been successfully unfollowed profile',
+  })
   @Delete(':username/follow')
   @UseGuards(AuthGuard)
   public async unFollowProfile(
